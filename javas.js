df51ef6d28aa3ed9c421119de34bb2bc22f61e93
Maram Abu Murad <maramabumurad97@gmail.com>
2021-04-25 21:18:28 +0300

    updates

---
+++ javas.js
@@ -0,0 +1,5 @@
+     let name = prompt("Welcome. Please enter your name?");
+     alert ( 'Welcome ' + name );
+
+     let x = prompt (" What's your favourite type( Rock,Pop,Alternative or Classics?)");
+     alert ("WOW! Nice one ")  
