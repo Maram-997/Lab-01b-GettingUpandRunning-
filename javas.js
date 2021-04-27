2e3bf1f6e1df1e7735691c376b1c509117548b15
Maram Abu Murad <maramabumurad97@gmail.com>
2021-04-28 02:07:51 +0300

    fixing issues

--- javas.js
+++ javas.js
@@ -1,5 +1,15 @@
      let name = prompt("Welcome. Please enter your name?");
      alert ( 'Welcome ' + name );
+     console.log(name);
 
      let x = prompt (" What's your favourite type( Rock,Pop,Alternative or Classics?)");
      alert ("WOW! Nice one ")  
+     console.log(x);
+
+     let y = prompt('Do You Enjoy working while listening to music?');
+     alert('For me all the time');
+     console.log(y);
+
+     let z = prompt('Since When You listen to music?');
+     alert('Really, That\'s Nice.')
+     console.log(z);
